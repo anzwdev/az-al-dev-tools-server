94d089a97e43b5694752e38f17189624bff28fc7
Andrzej Zwierzchowski <azwierzchowski@gmail.com>
2020-04-03 10:44:02 +0100

    Tests update

--- AZALDevToolsServer/AZALDevToolsTestConsoleApp/Program.cs
+++ AZALDevToolsServer/AZALDevToolsTestConsoleApp/Program.cs
@@ -19,10 +19,16 @@ namespace AZALDevToolsTestConsoleApp
             // The code provided will print ‘Hello World’ to the console.
             // Press Ctrl+F5 (or go to Debug > Start Without Debugging) to run your app.
 
-            ALDevToolsServer server = new ALDevToolsServer("C:\\Users\\azwie\\.vscode-insiders\\extensions\\ms-dynamics-smb.al-5.0.236243");
+            ALDevToolsServer server = new ALDevToolsServer("C:\\Users\\azwie\\.vscode\\extensions\\ms-dynamics-smb.al-5.0.254558");
 
             ALExtensionProxy alExtensionProxy = server.ALExtensionProxy;
             //alExtensionProxy.Load();
+
+            ALPackageSymbolsLibrary lib = new ALPackageSymbolsLibrary(alExtensionProxy,
+                "C:\\Projects\\Sandboxes\\al-test-projects\\SmallBC16\\.alpackages\\Microsoft_Base Application_16.0.11240.12076.app");
+            lib.Load(false);
+
+
 
             CodeAnalyzersLibrary library = server.CodeAnalyzersLibraries.GetCodeAnalyzersLibrary("${AppSourceCop}");
 
