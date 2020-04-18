55c7f18cec090753f3d0873fda8132d3ebceb8e7
Andrzej Zwierzchowski <azwierzchowski@gmail.com>
2020-04-19 00:08:35 +0100

    GetFullSyntaxTree message added

---
+++ AZALDevToolsServer/AZALDevToolsServer/Contracts/GetFullSyntaxTreeResponse.cs
@@ -0,0 +1,14 @@
+﻿using AnZwDev.ALTools.ALSymbols;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AZALDevToolsServer.Contracts
+{
+    public class GetFullSyntaxTreeResponse
+    {
+        public ALFullSyntaxTreeNode root { get; set; }
+    }
+}
---
+++ AZALDevToolsServer/AnZwDev.ALTools/ALSymbols/ALFullSyntaxTree.cs
@@ -0,0 +1,35 @@
+﻿using AnZwDev.ALTools.ALProxy;
+using AnZwDev.ALTools.ALSymbols.SymbolReaders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnZwDev.ALTools.ALSymbols
+{
+    public class ALFullSyntaxTree
+    {
+
+        public ALExtensionProxy ALExtensionProxy { get; }
+        public ALFullSyntaxTreeNode Root { get; set; }
+
+        public ALFullSyntaxTree(ALExtensionProxy alExtensionProxy)
+        {
+            this.ALExtensionProxy = alExtensionProxy;
+        }
+
+        public void Load(string source, string filePath)
+        {
+            ALFullSyntaxTreeReader reader = new ALFullSyntaxTreeReader(this.ALExtensionProxy);
+            if (!String.IsNullOrEmpty(source))
+                this.Root = reader.ProcessSourceCode(source);
+            else
+                this.Root = reader.ProcessSourceFile(filePath);
+                     
+
+        }
+
+
+    }
+}
